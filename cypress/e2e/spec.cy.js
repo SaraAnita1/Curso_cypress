5e6e4b37a96f22fb160ecda17768ed42a93367fc
Sara Anita <sara.anita.desa@gmail.com>
2025-03-03 18:56:13 -0300

    feat: criando primeiro cenario de testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.contains('a', 'Cadastrar').click();
+    cy.get('input[name="nome"]').type('Sara');
+    cy.get('input[name="email"]').type('sara.anita.desa@gmail.com');
+    cy.get('input[name="password"]').type('Senha123');
+    cy.get('input[name="confirm_password"]').type('Senha123');
+    cy.contains('button', 'Cadastrar').click();
   })
 })
